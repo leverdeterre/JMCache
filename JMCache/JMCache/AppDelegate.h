0e8d91218c6945442fefdac45a40e14a71d0b09b
jerome.morissard <morissardj@gmail.com>
2014-07-28 22:17:40 +0200

    project init

---
+++ JMCache/JMCache/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JMCache
+//
+//  Created by jerome morissard on 28/07/14.
+//  Copyright (c) 2014 jerome morissard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JMCache/JMCache/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JMCache
+//
+//  Created by jerome morissard on 28/07/14.
+//  Copyright (c) 2014 jerome morissard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
